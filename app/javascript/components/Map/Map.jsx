41cb5c59b7d59a32bc1de41a797d6192419e5053
Anshuman Bora <anshumanbora04@gmail.com>
2021-03-22 02:19:35 -0500

    Add Map and Search components

--- app/javascript/components/Map/Map.jsx
+++ app/javascript/components/Map/Map.jsx
@@ -1,7 +1,154 @@
-import React,{PureComponent} from 'react';
+import React, { PureComponent, createRef } from 'react'
+import SearchBar from './SearchBar'
+import SearchResultList from './SearchList'
+import './Map.css'
 
-export default class Map extends PureComponent{
-    render(){
-        return(<div>Map is here</div>)
+const getATGoogleAPIKey = () => "AIzaSyDIKzjfQQCahwJ9yEr8gBU9TqJ3MvbPXyY"
+
+export default class Map extends PureComponent {
+    constructor(props){
+        super(props);
+        this.createGoogleMap = this.createGoogleMap.bind(this);
+        this.getPlaces = this.getPlaces.bind(this);
+        this.displayMarkers = this.displayMarkers.bind(this)
+        this.state = {
+            responseList : null,
+            bounds : null,
+            map: null,
+            zoom: this.props.zoom
+        }
     }
-}
+
+    googleMapRef = React.createRef()
+    
+    componentDidMount() {
+        const location = {
+            lat: 41.4566,
+            lng: 93.4332,
+          }
+        let that = this;
+        setTimeout(()=>{
+            that.createGoogleMap()  
+            that.getPlaces(location)    
+        },200)  
+        
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate');
+    }
+    static getDerivedStateFromProps(props,state){
+        let newState = state;
+        console.log("getDerivedStateFromProps")
+        return newState;
+    }
+    handleMapClick= () => {
+        console.log('click')
+    }
+    createGoogleMap(){
+      let map = new window.google.maps.Map(this.googleMapRef.current, {
+        zoom: this.state.zoom,
+        center: {
+          lat: 41.345456,
+          lng: -93.12433,
+        },
+        disableDefaultUI: true,
+      })
+      map.addListener('center_changed', () => {
+        let bounds = map.getBounds()
+        let center = map.getCenter()
+     this.handleCenterChange(bounds, center)
+    });
+      this.setState({map:map})
+    //   console.log('creating new map and setting state map')
+    }
+    handleCenterChange = (bounds, center)=> {
+        let newLocation = {
+            lat: center.lat(),
+            lng: center.lng()
+        }
+        // console.log("calling getplaces")
+        this.getPlaces(newLocation)
+    }
+    getPlaces(location){
+        // TODO 1. pagination to get more places(look into it)
+        // TODO 2. Get nearby query from bounding box instead of center of the map
+        const request = {
+            radius: 1000,
+            type: ['restaurant'],
+            location: location
+          };
+            
+          if(this.state.map){
+            console.log("1 getting nearby places for location:",location)
+            let service = new window.google.maps.places.PlacesService(this.state.map);
+            service.nearbySearch(request,(results,status)=>{
+                console.log(status)
+            if(status=="OK"){
+                console.log(results);
+                this.displayMarkers(results, this.state.map,location)
+                this.setState({responseList:<SearchResultList responseList={results}/>})
+            }
+          })
+          }
+          else {
+            console.log("2 getting nearby places for location:",location)
+        
+              let that = this
+              setTimeout(()=>{
+                  let service = new window.google.maps.places.PlacesService(that.state.map);
+                  service.nearbySearch(request,(results,status)=>{
+                  if(status=="OK"){
+                      console.log(results);
+                      that.displayMarkers(results, that.state.map,location)
+                      that.setState({responseList:<SearchResultList responseList={results}/>})
+                  }
+                })         
+                },1000)
+          }
+          
+    }
+    displayMarkers(locationList, map, location){
+        
+        for(let i=0;i<locationList.length;i++){
+            const pos = locationList[i].geometry.location
+            const name = locationList[i].name
+            // console.log("name:",name,",location:",pos)
+            // TODO change icon of markers
+            new window.google.maps.Marker({
+                position: pos,
+                map,
+                title: name,
+              });
+        }
+    }
+    handleSearch = (searchTerm)=>{
+        // console.log('This came from child:', searchTerm)
+    }    
+    // TODO handle quick refresh of map. Quick refresh leads to the app crashing because the 
+    //google object is not ready yet
+    render() {
+        // console.log(this.state.map)
+        let searchResultList = this.state.responseList?this.state.responseList:<div>No Results for this Area</div>
+      return (
+      <div className="TopLevelWrapper">
+            <div>
+                <SearchBar
+                    onSearch={this.handleSearch}
+                />
+            </div>
+            <div className="SearchListAndMapWrapper">
+                <div className="SearchListWrapper">
+                {searchResultList}
+                </div>
+                <div
+                    id="google-map"
+                    ref={this.googleMapRef}
+                    style={{ width: '100%', height: '800px' }}>
+                </div>
+            </div>
+            
+      </div>
+      )
+    }
+  }
+
---
+++ app/javascript/components/Map/SearchList.jsx
@@ -0,0 +1,85 @@
+import React,{PureComponent} from "react";
+import './SearchList.css'
+
+export default class SearchResultList extends PureComponent{
+    constructor(props){
+        super(props)
+    }
+    handleFavoriteClick = () => {
+        console.log("fav clicked");
+    }
+    createStarRating(ratings){
+        // TODO: create star rating thingy
+        let active_star = <img src="/static/assets/star.png" class="Rating--Star Rating--Star__active"></img>
+        let passive_star =  <img src="/static/assets/star.png" class="Rating--Star"></img> 
+        
+        let stars = null;  
+        if(ratings){
+            let i=0;
+            while(i<ratings){
+                return (active_star)
+                i+=1
+            }
+            while(i<ratings){
+                ratingDiv.join(passive_star);
+                i+=1
+            }
+        }
+
+    let ratingDiv = <div class="Rating" aria-label="Rating of this item is 3 out of 5"></div>
+        console.log(ratingDiv)
+        return 
+    }
+    createUnitView = (restaurant)=>{
+        // TODO Maybe get the price from making individual
+        // API call to place details request
+        let photo=<img width="100" height="100" alt="No images found" src="https://via.placeholder.com/50"></img>
+        if(restaurant.photos){
+            photo = <img 
+            src={restaurant.photos[0].getUrl()}
+            width="100" height="100"
+            ></img>
+        }
+        let favoriteIcon = <svg onClick={this.handleFavoriteClick} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+            <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+        </svg>
+        let user_ratings_total = restaurant.user_ratings_total?restaurant.user_ratings_total:"No data"
+        console.log(user_ratings_total)
+    
+        // let start_rating = this.createStarRating(restaurant.rating)
+        return(
+        <div className="UnitView"
+        key={restaurant.place_id}>
+            <div className="RestaurantPhoto">{photo}</div>
+            <div className="RestaurantDetails">
+                <div className="RestaurantName">{restaurant.name}</div>
+                <div className="Ratings">
+                    <div className="RestaurantRating">{restaurant.start_rating}</div>
+                    <div className="RestaurantTotalRating">({user_ratings_total})</div>
+                </div>
+            </div>
+            <div className="Favorite">{favoriteIcon}</div>
+            
+        </div>)
+    }
+    createRestaurantList = (responseList) =>{
+        let listOfRestaurants = []
+        for(let i=0;i<responseList.length;i++){
+            console.log("pushing:", responseList[i])
+            listOfRestaurants.push(
+                this.createUnitView(responseList[i])
+                )
+        }
+        return listOfRestaurants;
+    }
+    render(){
+        // console.log('inside list making')
+        let listOfRestaurants = this.createRestaurantList(this.props.responseList)
+        // console.log(list)
+        return(
+            <div className="SearchResultListContainer">
+                {listOfRestaurants}
+            </div>
+        )
+    }
+}
