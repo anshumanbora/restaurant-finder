f4383188424026163252153dde9b2427139b12a0
Anshuman Bora <anshumanbora04@gmail.com>
2021-03-24 01:22:35 -0500

    Test run prettier formatting tool

--- app/javascript/components/Map/Map.jsx
+++ app/javascript/components/Map/Map.jsx
@@ -1,233 +1,228 @@
 // TODO: get some linting tool to make codebase style uniform
-import React, { PureComponent, createRef } from 'react'
-import SearchBar from './SearchBar'
-import SearchResultList from './SearchList'
-import './Map.css'
+import React, { PureComponent, createRef } from "react";
+import SearchBar from "./SearchBar";
+import SearchResultList from "./SearchList";
+import "./Map.css";
 
 export default class Map extends PureComponent {
-    constructor(props){
-        super(props);
-        this.googleMapRef = React.createRef()
-        this.state = {
-            responseList : null,
-            bounds : null,
-            map: null,
-            zoom: 14,
-            center: {
-                lat: 37.773972,
-                lng: -122.431297,
-            },
-            markersList : [],
-            favorites:null,
-            searchTerm: null,
-            mobileView: false,
-            toggleMap: true,
-            mapHeight: '800'
-        }
-    }
-
-    // TODO change default location of map. Maybe make it more dynamic?
-    // TODO remove locations from list if not inside bounds
-    componentDidMount() {
-        let that = this;
-        setTimeout(()=>{
-            that.createGoogleMap()    
-        },200)
-        window.addEventListener("resize", this.handleResize.bind(this));  
-        this.handleResize();
-        // const height = document.getElementById('MapWrapperId').clientHeight;
-        // let mapHeight = ''+height;
-        // this.setState({ mapHeight });
-    }   
-    componentDidUpdate(){
-        console.log('did update',this.state.searchTerm)
-        this.getPlaces()
-    }
-    
-    saveFavorite= (favorites) => {
-        this.setState({favorites})
-    }
-
-    createGoogleMap =()=>{
-      let map = new window.google.maps.Map(this.googleMapRef.current, {
-            zoom: this.state.zoom,
-            center: this.state.center,
-            disableDefaultUI: true,
-        })
-        map.addListener('center_changed', () => {
-            let center = map.getCenter()
-            this.handleCenterChange(center)
-        });
-        this.setState({map:map})
-    }
-    handleCenterChange = ()=> {
-        let responseList = null;
-        this.setState({responseList})
-        this.getPlaces();
-    }
-    
-    validateLocations = (locationList) =>{
-        let bounds = this.state.map.getBounds();
-        let validatedResponse = []
-        for(let i=0;i<locationList.length;i++){
-            let loc = locationList[i].geometry.location;
-            if(
-                loc.lat()<bounds.Sa.i 
-                && loc.lat()>bounds.Sa.g
-                && loc.lng()<bounds.La.i
-                && loc.lng()>bounds.La.g
-            ){
-                validatedResponse.push(locationList[i])
-            }
-        }
-        return validatedResponse;
-    }
-    getPlaces = () => {
-        // TODO  Make this more modular. Break it in to maintainable components
-        let {searchTerm, map, markersList} = this.state;
-        //search empty and there are markers present
-        //so just clear them
-        if(map && searchTerm === '' && markersList){
-            this.handleMarkers()
-        }
-        if(map && searchTerm){  
-            let request = {
-                query: searchTerm,
-                location: map.getCenter(),
-                fields: [
-                    'name',
-                    'geometry', 
-                    'place_id',
-                    'photos',
-                    'price_level',
-                    'rating',
-                    'user_ratings_total',
-                   
-                ],
-            };
-            let service = new window.google.maps.places.PlacesService(this.state.map);
-            service.textSearch(request, (results,status)=>{
-                if(status=="OK"){    
-                    let validatedResults = this.validateLocations(results);
-                    if(validatedResults.length>0){
-                        this.handleMarkers(validatedResults, map)
-                    }
-                    this.setState(
-                        {
-                            responseList:
-                            <SearchResultList getFavorite={this.saveFavorite} responseList={validatedResults}/>
-                        }
-                    );
-                    
-                }
-            });
-        }         
-    }
-    handleMarkers = (locationList, map) => {
-        let markersList = this.state.markersList
-        // Clearing existing markers before rendering new ones.
-        // A future improvement would 
-        // be to break these into two functions, one more clearing and the other
-        // for creating. 
-        if(markersList){
-            for (let i = 0; i < markersList.length; i++) {
-                markersList[i].setMap(null);
-            }
-        } 
-        // Sike! No locations to mark so return from here
-        if(!locationList){
-            return;
-        }
-        for(let i=0;i<locationList.length;i++){
-            const pos = locationList[i].geometry.location
-            const name = locationList[i].name
-            const marker = new window.google.maps.Marker({
-                position: pos,
-                map,
-                title: name,
-              });  
-            markersList.push(marker) 
-        }
-        this.setState({markersList:markersList})
-        this.setMapOnAll(map)
-    }
-    setMapOnAll =(map)=>{
-        let markersList = this.state.markersList
-        if(markersList){
-            for (let i = 0; i < markersList.length; i++) {
-                markersList[i].setMap(map);
-            }
-        }        
-    }
-    
-    handleSearch = (searchTerm)=>{
-        let responseList = null;
-        this.setState({searchTerm, responseList});
-    }    
-    handleResize = () => {
-        let mobileView = false;
-        if(window.innerWidth<865){
-            mobileView = true; 
-        }
-        this.setState({mobileView})
-    } 
-    toggleMapInMobile = () => {
-        let toggleMap = !this.state.toggleMap; 
-        this.setState({toggleMap})
-    }
-    render() {
-        let {responseList, mobileView, toggleMap, mapHeight, map} = this.state;
-        let searchResultList = responseList ? responseList:<div></div>
-        let searchResultlistClass = "SearchListWrapper"
-        let toggleButtonName = ""
-        let toggleButtonClass = "Hide"
-        let SearchListWidth = "100%" 
-        if(!responseList){
-            searchResultlistClass = "Hide"
-        }
-        if(mobileView){
-            toggleButtonClass = "ToggleButton Show"
-            if(toggleMap){
-                searchResultlistClass = "SearchListWrapper Hide"
-                toggleButtonName = "List"
-            }else{
-                searchResultlistClass = "SearchListWrapper"
-                toggleButtonName = "Map"
-                SearchListWidth = window.innerWidth+"px"
-            }
-        }
-        let mobileToggleButton =   <button 
-                                        className={toggleButtonClass}
-                                        onClick={this.toggleMapInMobile}
-                                    >{toggleButtonName}
-                                    </button> 
-
-      return (
-      <div className="TopLevelWrapper">
-            <div>
-                <SearchBar
-                    onSearch={this.handleSearch}
-                />
-            </div>
-            <div
-                id="MapWrapperId" 
-                className="SearchListAndMapWrapper">
-                <div 
-                    style={{width: SearchListWidth}}
-                    className={searchResultlistClass}>
-                    {searchResultList}
-                </div>
-                <div
-                    id="google-map"
-                    ref={this.googleMapRef}
-                    style={{ width: '100%', height: {mapHeight} }}
-                    className="MapWrapper"
-                    >   
-                </div>
-                {mobileToggleButton}
-            </div>        
-      </div>
-      )
-    }
+  constructor(props) {
+    super(props);
+    this.googleMapRef = React.createRef();
+    this.state = {
+      responseList: null,
+      bounds: null,
+      map: null,
+      zoom: 14,
+      center: {
+        lat: 37.773972,
+        lng: -122.431297,
+      },
+      markersList: [],
+      favorites: null,
+      searchTerm: null,
+      mobileView: false,
+      toggleMap: true,
+      mapHeight: "800",
+    };
   }
 
+  // TODO change default location of map. Maybe make it more dynamic?
+  // TODO remove locations from list if not inside bounds
+  componentDidMount() {
+    let that = this;
+    setTimeout(() => {
+      that.createGoogleMap();
+    }, 200);
+    window.addEventListener("resize", this.handleResize.bind(this));
+    this.handleResize();
+    // const height = document.getElementById('MapWrapperId').clientHeight;
+    // let mapHeight = ''+height;
+    // this.setState({ mapHeight });
+  }
+  componentDidUpdate() {
+    console.log("did update", this.state.searchTerm);
+    this.getPlaces();
+  }
+
+  saveFavorite = (favorites) => {
+    this.setState({ favorites });
+  };
+
+  createGoogleMap = () => {
+    let map = new window.google.maps.Map(this.googleMapRef.current, {
+      zoom: this.state.zoom,
+      center: this.state.center,
+      disableDefaultUI: true,
+    });
+    map.addListener("center_changed", () => {
+      let center = map.getCenter();
+      this.handleCenterChange(center);
+    });
+    this.setState({ map: map });
+  };
+  handleCenterChange = () => {
+    let responseList = null;
+    this.setState({ responseList });
+    this.getPlaces();
+  };
+
+  validateLocations = (locationList) => {
+    let bounds = this.state.map.getBounds();
+    let validatedResponse = [];
+    for (let i = 0; i < locationList.length; i++) {
+      let loc = locationList[i].geometry.location;
+      if (
+        loc.lat() < bounds.Sa.i &&
+        loc.lat() > bounds.Sa.g &&
+        loc.lng() < bounds.La.i &&
+        loc.lng() > bounds.La.g
+      ) {
+        validatedResponse.push(locationList[i]);
+      }
+    }
+    return validatedResponse;
+  };
+  getPlaces = () => {
+    // TODO  Make this more modular. Break it in to maintainable components
+    let { searchTerm, map, markersList } = this.state;
+    //search empty and there are markers present
+    //so just clear them
+    if (map && searchTerm === "" && markersList) {
+      this.handleMarkers();
+    }
+    if (map && searchTerm) {
+      let request = {
+        query: searchTerm,
+        location: map.getCenter(),
+        fields: [
+          "name",
+          "geometry",
+          "place_id",
+          "photos",
+          "price_level",
+          "rating",
+          "user_ratings_total",
+        ],
+      };
+      let service = new window.google.maps.places.PlacesService(this.state.map);
+      service.textSearch(request, (results, status) => {
+        if (status == "OK") {
+          let validatedResults = this.validateLocations(results);
+          if (validatedResults.length > 0) {
+            this.handleMarkers(validatedResults, map);
+          }
+          this.setState({
+            responseList: (
+              <SearchResultList
+                getFavorite={this.saveFavorite}
+                responseList={validatedResults}
+              />
+            ),
+          });
+        }
+      });
+    }
+  };
+  handleMarkers = (locationList, map) => {
+    let markersList = this.state.markersList;
+    // Clearing existing markers before rendering new ones.
+    // A future improvement would
+    // be to break these into two functions, one more clearing and the other
+    // for creating.
+    if (markersList) {
+      for (let i = 0; i < markersList.length; i++) {
+        markersList[i].setMap(null);
+      }
+    }
+    // Sike! No locations to mark so return from here
+    if (!locationList) {
+      return;
+    }
+    for (let i = 0; i < locationList.length; i++) {
+      const pos = locationList[i].geometry.location;
+      const name = locationList[i].name;
+      const marker = new window.google.maps.Marker({
+        position: pos,
+        map,
+        title: name,
+      });
+      markersList.push(marker);
+    }
+    this.setState({ markersList: markersList });
+    this.setMapOnAll(map);
+  };
+  setMapOnAll = (map) => {
+    let markersList = this.state.markersList;
+    if (markersList) {
+      for (let i = 0; i < markersList.length; i++) {
+        markersList[i].setMap(map);
+      }
+    }
+  };
+
+  handleSearch = (searchTerm) => {
+    let responseList = null;
+    this.setState({ searchTerm, responseList });
+  };
+  handleResize = () => {
+    let mobileView = false;
+    if (window.innerWidth < 865) {
+      mobileView = true;
+    }
+    this.setState({ mobileView });
+  };
+  toggleMapInMobile = () => {
+    let toggleMap = !this.state.toggleMap;
+    this.setState({ toggleMap });
+  };
+  render() {
+    let { responseList, mobileView, toggleMap, mapHeight, map } = this.state;
+    let searchResultList = responseList ? responseList : <div></div>;
+    let searchResultlistClass = "SearchListWrapper";
+    let toggleButtonName = "";
+    let toggleButtonClass = "Hide";
+    let SearchListWidth = "100%";
+    if (!responseList) {
+      searchResultlistClass = "Hide";
+    }
+    if (mobileView) {
+      toggleButtonClass = "ToggleButton Show";
+      if (toggleMap) {
+        searchResultlistClass = "SearchListWrapper Hide";
+        toggleButtonName = "List";
+      } else {
+        searchResultlistClass = "SearchListWrapper";
+        toggleButtonName = "Map";
+        SearchListWidth = window.innerWidth + "px";
+      }
+    }
+    let mobileToggleButton = (
+      <button className={toggleButtonClass} onClick={this.toggleMapInMobile}>
+        {toggleButtonName}
+      </button>
+    );
+
+    return (
+      <div className="TopLevelWrapper">
+        <div>
+          <SearchBar onSearch={this.handleSearch} />
+        </div>
+        <div id="MapWrapperId" className="SearchListAndMapWrapper">
+          <div
+            style={{ width: SearchListWidth }}
+            className={searchResultlistClass}
+          >
+            {searchResultList}
+          </div>
+          <div
+            id="google-map"
+            ref={this.googleMapRef}
+            style={{ width: "100%", height: { mapHeight } }}
+            className="MapWrapper"
+          ></div>
+          {mobileToggleButton}
+        </div>
+      </div>
+    );
+  }
+}
--- app/javascript/components/Map/SearchList.jsx
+++ app/javascript/components/Map/SearchList.jsx
@@ -1,135 +1,161 @@
-import React,{PureComponent} from "react";
-import './SearchList.css'
-import StarRatings from 'react-star-ratings';
+import React, { PureComponent } from "react";
+import "./SearchList.css";
+import StarRatings from "react-star-ratings";
 
-export default class SearchResultList extends PureComponent{
-    constructor(props){
-        super(props)
-        this.state = {
-            restaurantList: null,
-            favoriteList : [],
-            refresh: null
-        }
+export default class SearchResultList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurantList: null,
+      favoriteList: [],
+      refresh: null,
+    };
+  }
+  handleFavoriteClick = (placeId) => {
+    let favoriteList = this.state.favoriteList;
+    if (favoriteList.includes(placeId)) {
+      favoriteList.pop(placeId);
+    } else {
+      favoriteList.push(placeId);
     }
-    handleFavoriteClick = (placeId) => {
-        let favoriteList = this.state.favoriteList;
-        if(favoriteList.includes(placeId)){
-            favoriteList.pop(placeId)
-        }else{
-            favoriteList.push(placeId)
-        }
-        let refresh = this.state.refresh?false:true;
-        // console.log("clicked heart, setting state", this.state)
-        this.setState({favoriteList:favoriteList, refresh:true})
-        let restaurantList = this.createRestaurantList(this.props.responseList)
-        this.setState({restaurantList:restaurantList})
-        
-        this.props.getFavorite(favoriteList)
-    }
-    createStarRating(ratings){
-        return <StarRatings
-          rating={ratings}
-          starRatedColor="rgb(218,165,32)"
-          numberOfStars={5}
-          starDimension="15px"
-          starSpacing="1px"
-          name='rating'
-        />
-        
-    }
-    generateFavoriteIcon = (placeId)=>{
-        let favorites = this.state.favoriteList;
-        if(favorites.includes(placeId)){
-            // console.log('exists')
-            return  <svg onClick={()=>this.handleFavoriteClick(placeId)} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-heart-fill" viewBox="0 0 16 16">
-                        <path fillRule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                    </svg>
-        }
-        // console.log('doesn\'t exists')
-            
-        return  <svg onClick={()=>this.handleFavoriteClick(placeId)} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
-                    <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
-                </svg>
-    }
-    generatePriceLevel = (priceLevel) => {
-        // console.log(priceLevel)
-        let dollars = '$';
-        if(priceLevel>3){
-            dollars+='$$'
-        }else if(priceLevel>1){
-            dollars+='$'
-        }
-        return dollars
-    }
-    generatePhoto = (photos) => {
-        let photo=<img width="100" height="100" alt="No images found" src="https://via.placeholder.com/50"></img>
-        if(photos){
-            photo = <img 
-            src={photos[0].getUrl()}
-            width="100" height="100"
-            ></img>
-        }
-        return photo
-    }
-    createUnitView = (restaurant)=>{
-        // console.log(restaurant)
-        // TODO Maybe get the price from making individual
-        // API call to place details request
+    let refresh = this.state.refresh ? false : true;
+    // console.log("clicked heart, setting state", this.state)
+    this.setState({ favoriteList: favoriteList, refresh: true });
+    let restaurantList = this.createRestaurantList(this.props.responseList);
+    this.setState({ restaurantList: restaurantList });
 
-        let photo = this.generatePhoto(restaurant.photos)
-        let favoriteIcon = this.generateFavoriteIcon(restaurant.place_id);
-        let userRatingTotal = restaurant.user_ratings_total?restaurant.user_ratings_total:"No data"
-        let startRating = this.createStarRating(restaurant.rating)
-        let priceRating = this.generatePriceLevel(restaurant.price_level)
-        return(
-        <div className="UnitView"
-        key={restaurant.place_id}>
-            <div className="RestaurantPhoto">{photo}</div>
-            <div className="RestaurantDetails">
-                <div className="RestaurantName">{restaurant.name}</div>
-                <div className="Ratings">
-                    <div className="PriceRating">{priceRating}</div>
-                    <div className="RestaurantRating">{startRating}</div>
-                    <div className="RestaurantTotalRating">({userRatingTotal})</div>
-                </div>
-            </div>
-            <div className="Favorite">{favoriteIcon}</div>
-            
-        </div>)
+    this.props.getFavorite(favoriteList);
+  };
+  createStarRating(ratings) {
+    return (
+      <StarRatings
+        rating={ratings}
+        starRatedColor="rgb(218,165,32)"
+        numberOfStars={5}
+        starDimension="15px"
+        starSpacing="1px"
+        name="rating"
+      />
+    );
+  }
+  generateFavoriteIcon = (placeId) => {
+    let favorites = this.state.favoriteList;
+    if (favorites.includes(placeId)) {
+      // console.log('exists')
+      return (
+        <svg
+          onClick={() => this.handleFavoriteClick(placeId)}
+          xmlns="http://www.w3.org/2000/svg"
+          width="30"
+          height="30"
+          fill="currentColor"
+          className="bi bi-heart-fill"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+          />
+        </svg>
+      );
     }
-    createRestaurantList = (responseList) =>{
-        let listOfRestaurants = []
-        for(let i=0;i<responseList.length;i++){
-            listOfRestaurants.push(
-                this.createUnitView(responseList[i])
-                )
-        }
-        return listOfRestaurants;
+    // console.log('doesn\'t exists')
+
+    return (
+      <svg
+        onClick={() => this.handleFavoriteClick(placeId)}
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        fill="currentColor"
+        className="bi bi-heart"
+        viewBox="0 0 16 16"
+      >
+        <path d="M8 2.748l-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+      </svg>
+    );
+  };
+  generatePriceLevel = (priceLevel) => {
+    // console.log(priceLevel)
+    let dollars = "$";
+    if (priceLevel > 3) {
+      dollars += "$$";
+    } else if (priceLevel > 1) {
+      dollars += "$";
     }
-    componentDidUpdate = (prevProps) =>{
-        // Prevent infinite calls to render by setting state only when 
-        // a new prop comes in
-        if(prevProps.responseList !== this.props.responseList){
-            let restaurantList = this.createRestaurantList(this.props.responseList)
-            this.setState({restaurantList:restaurantList})
-        }
+    return dollars;
+  };
+  generatePhoto = (photos) => {
+    let photo = (
+      <img
+        width="100"
+        height="100"
+        alt="No images found"
+        src="https://via.placeholder.com/50"
+      ></img>
+    );
+    if (photos) {
+      photo = <img src={photos[0].getUrl()} width="100" height="100"></img>;
     }
-    componentDidMount = () => {
-        let restaurantList = this.createRestaurantList(this.props.responseList)
-        this.setState({restaurantList:restaurantList})
+    return photo;
+  };
+  createUnitView = (restaurant) => {
+    // console.log(restaurant)
+    // TODO Maybe get the price from making individual
+    // API call to place details request
+
+    let photo = this.generatePhoto(restaurant.photos);
+    let favoriteIcon = this.generateFavoriteIcon(restaurant.place_id);
+    let userRatingTotal = restaurant.user_ratings_total
+      ? restaurant.user_ratings_total
+      : "No data";
+    let startRating = this.createStarRating(restaurant.rating);
+    let priceRating = this.generatePriceLevel(restaurant.price_level);
+    return (
+      <div className="UnitView" key={restaurant.place_id}>
+        <div className="RestaurantPhoto">{photo}</div>
+        <div className="RestaurantDetails">
+          <div className="RestaurantName">{restaurant.name}</div>
+          <div className="Ratings">
+            <div className="PriceRating">{priceRating}</div>
+            <div className="RestaurantRating">{startRating}</div>
+            <div className="RestaurantTotalRating">({userRatingTotal})</div>
+          </div>
+        </div>
+        <div className="Favorite">{favoriteIcon}</div>
+      </div>
+    );
+  };
+  createRestaurantList = (responseList) => {
+    let listOfRestaurants = [];
+    for (let i = 0; i < responseList.length; i++) {
+      listOfRestaurants.push(this.createUnitView(responseList[i]));
     }
-    render(){
-        let {restaurantList} = this.state;
-        if(restaurantList && restaurantList.length==0){
-            return <div className="SearchResultListContainer">
-                        "No results for this search. Try widening your map area or a different search term"
-                    </div>
-        }
-        let listOfRestaurants = this.state.restaurantList;
-        return(
-            <div className="SearchResultListContainer">
-                {listOfRestaurants}
-            </div>
-        )
+    return listOfRestaurants;
+  };
+  componentDidUpdate = (prevProps) => {
+    // Prevent infinite calls to render by setting state only when
+    // a new prop comes in
+    if (prevProps.responseList !== this.props.responseList) {
+      let restaurantList = this.createRestaurantList(this.props.responseList);
+      this.setState({ restaurantList: restaurantList });
     }
+  };
+  componentDidMount = () => {
+    let restaurantList = this.createRestaurantList(this.props.responseList);
+    this.setState({ restaurantList: restaurantList });
+  };
+  render() {
+    let { restaurantList } = this.state;
+    if (restaurantList && restaurantList.length == 0) {
+      return (
+        <div className="SearchResultListContainer">
+          "No results for this search. Try widening your map area or a different
+          search term"
+        </div>
+      );
+    }
+    let listOfRestaurants = this.state.restaurantList;
+    return <div className="SearchResultListContainer">{listOfRestaurants}</div>;
+  }
 }
